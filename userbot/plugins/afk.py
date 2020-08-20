e95e5f390254dc7a94163e2825ad9bd85a2cf073
IhsanaZay <67726343+IhsanaZay@users.noreply.github.com>
2020-08-20 10:43:15 +0700

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -107,7 +107,7 @@ async def on_afk(event):
         message_to_reply = f"__My Master Has Been In afk For__ `{total_afk_time}`\nWhere He Is: ONLY GOD KNOWS " + \
             f"\n\n__I promise He'll back in a few light years__\n**REASON**: {reason}" \
             if reason \
-            else f"**Heya!**\n__I am currently unavailable. Since when, you ask? For {total_afk_time} I guess.__\n\nWhen will I be back? Soon __Whenever I feel like it__**( ಠ ʖ̯ ಠ)**  "
+            else f"**Hallo!**\n__Saya saat sedang tidak aktif. Sejak {total_afk_time} .__\n\nKapan saya akan kembali? Segera __ Setiap kali saya merasa seperti itu__**( ಠ ʖ̯ ಠ)**  "
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
         if event.chat_id in last_afk_message:  # pylint:disable=E0602
